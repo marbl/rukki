59145d50706022641dd27ae711ddbd57860875b4
Sergey Nurk <sergeynurk@gmail.com>
2022-01-20 11:52:22 -0500

    add dead-ends to bounded dfs border

--- src/graph_algos.rs
+++ src/graph_algos.rs
@@ -5,16 +5,20 @@ pub mod graph_algos {
 use crate::graph::*;
 use std::collections::HashSet;
 
-fn inner_dfs(g: &Graph, v: Vertex, node_len_thr: usize, visited: &mut HashSet<Vertex>, long_ext: &mut Vec<Vertex>) {
+fn inner_dfs(g: &Graph, v: Vertex, node_len_thr: usize,
+             visited: &mut HashSet<Vertex>, border: &mut Vec<Vertex>) {
     visited.insert(v);
     //if only one vertex is visited then it means we just started
     if visited.len() > 1 && g.node(v.node_id).length >= node_len_thr {
-        long_ext.push(v);
+        border.push(v);
     } else {
+        if g.outgoing_edge_cnt(v) == 0 {
+            border.push(v);
+        }
         for l in g.outgoing_edges(v) {
             let w = l.end;
             if !visited.contains(&w) {
-                inner_dfs(g, w, node_len_thr, visited, long_ext);
+                inner_dfs(g, w, node_len_thr, visited, border);
             }
         }
     }
@@ -24,8 +28,8 @@ pub fn bounded_dfs(g: &Graph, v: Vertex, node_len_thr: usize)
     -> (HashSet<Vertex>, Vec<Vertex>) {
     //TODO change for integer vectors
     let mut visited = HashSet::new();
-    let mut long_ext = Vec::new();
-    inner_dfs(g, v, node_len_thr, &mut visited, &mut long_ext);
-    (visited, long_ext)
+    let mut border = Vec::new();
+    inner_dfs(g, v, node_len_thr, &mut visited, &mut border);
+    (visited, border)
 }
 }
