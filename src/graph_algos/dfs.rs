9e20ae74a41a27cde9a8eaf22f5e03ea4e6f7418
Sergey Nurk <sergeynurk@gmail.com>
2022-02-17 16:33:13 -0500

    component search logic fix

--- src/graph_algos/dfs.rs
+++ src/graph_algos/dfs.rs
@@ -125,6 +125,7 @@ impl<'a> DFS<'a> {
 //includes boundary vertices (longer than threshold) and visited dead-ends
 //currently will include v if it's a dead-end (but not if it's longer than threshold)
 //returns pair of sinks and all ('inner') visited vertices
+//visited vertices will overlap sinks by short dead-ends
 pub fn sinks_ahead(g: &Graph, v: Vertex, node_len_thr: usize) -> (Vec<Vertex>, HashSet<Vertex>) {
     let mut dfs = DFS::new_forward(g);
     dfs.set_max_node_len(node_len_thr);
@@ -141,46 +142,62 @@ pub struct ShortNodeComponent {
     pub sources: HashSet<Vertex>,
     pub sinks: HashSet<Vertex>,
     pub has_deadends: bool,
-    pub reached: HashSet<Vertex>,
+    pub inner: HashSet<Vertex>,
 }
 
 impl ShortNodeComponent {
 
     fn consider(&mut self, g: &Graph, v: Vertex, l: Link, length_threshold: usize) {
-        if !self.reached.contains(&v) {
-            self.reached.insert(v);
+        let mut is_source = false;
+        let mut is_sink = false;
 
-            if g.vertex_length(v) >= length_threshold
-                && v == l.start {
-                //if v is long and we came into its end then it's a source
-                self.sources.insert(v);
-            } else {
-                //otherwise consider it's incoming edges
-                if g.incoming_edge_cnt(v) == 0 {
-                    assert!(g.vertex_length(v) < length_threshold);
-                    self.has_deadends = true;
+        if g.vertex_length(v) < length_threshold {
+            if !self.inner.insert(v) {
+                //inner already considered
+                return;
+            }
+        } else {
+            //v is long
+            if v == l.start {
+                //if v is long and we came from the 'right'
+                is_source = true;
+                if !self.sources.insert(v) {
+                    //source already considered
+                    return;
                 }
-                for i_l in g.incoming_edges(v) {
-                    if i_l != l {
-                        self.consider(g, i_l.start, i_l, length_threshold);
-                    }
+            } else {
+                assert!(v == l.end);
+                //if v is long and we came from the 'left'
+                is_sink = true;
+                if !self.sinks.insert(v) {
+                    //sink already considered
+                    return;
                 }
             }
+        }
 
-            if g.vertex_length(v) >= length_threshold
-                && v == l.end {
-                //if v is long and we came into its start then it's a sink
-                self.sinks.insert(v);
-            } else {
-                //otherwise consider it's outgoing edges
-                if g.outgoing_edge_cnt(v) == 0 {
-                    assert!(g.vertex_length(v) < length_threshold);
-                    self.has_deadends = true;
+        //if not a source consider it's incoming edges
+        if !is_source {
+            if g.incoming_edge_cnt(v) == 0 {
+                assert!(g.vertex_length(v) < length_threshold);
+                self.has_deadends = true;
+            }
+            for i_l in g.incoming_edges(v) {
+                if i_l != l {
+                    self.consider(g, i_l.start, i_l, length_threshold);
                 }
-                for o_l in g.outgoing_edges(v) {
-                    if o_l != l {
-                        self.consider(g, o_l.end, o_l, length_threshold);
-                    }
+            }
+        }
+
+        //if not a sink consider outgoing edges
+        if !is_sink {
+            if g.outgoing_edge_cnt(v) == 0 {
+                assert!(g.vertex_length(v) < length_threshold);
+                self.has_deadends = true;
+            }
+            for o_l in g.outgoing_edges(v) {
+                if o_l != l {
+                    self.consider(g, o_l.end, o_l, length_threshold);
                 }
             }
         }
@@ -204,7 +221,7 @@ impl ShortNodeComponent {
             sources: std::iter::once(v).collect(),
             sinks: HashSet::new(),
             has_deadends: false,
-            reached: std::iter::once(v).collect(),
+            inner: HashSet::new(),
         };
 
         for o_l in g.outgoing_edges(v) {
@@ -219,7 +236,7 @@ impl ShortNodeComponent {
             sources: HashSet::new(),
             sinks: std::iter::once(v).collect(),
             has_deadends: false,
-            reached: std::iter::once(v).collect(),
+            inner: HashSet::new(),
         };
 
         for i_l in g.incoming_edges(v) {
@@ -238,7 +255,7 @@ impl ShortNodeComponent {
                 sources: HashSet::new(),
                 sinks: HashSet::new(),
                 has_deadends: (g.outgoing_edge_cnt(v) == 0 || g.incoming_edge_cnt(v) == 0),
-                reached: std::iter::once(v).collect(),
+                inner: std::iter::once(v).collect(),
             };
             for i_l in g.incoming_edges(v) {
                 component.consider(g, i_l.start, i_l, length_threshold);
@@ -248,5 +265,9 @@ impl ShortNodeComponent {
             }
             component
         }
+    }
+
+    pub fn all_nodes(&self) -> impl Iterator<Item=&Vertex> {
+        self.inner.iter().chain(self.sources.iter()).chain(self.sinks.iter())
     }
 }
